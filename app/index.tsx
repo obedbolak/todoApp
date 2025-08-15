6af686e3d27a9323af5f6d65102263737e88ecc4
obedbolak <jamesnormankate@gmail.com>
2025-08-15 13:51:22 +0100

    last update

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const index = () => {
+  return (
+    <TouchableOpacity>
+      <Text>index</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
